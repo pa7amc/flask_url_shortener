9c7f1bfe8c1b91c9a80ba78291c879981281575c
pa7amc <pa7amc@gmail.com>
2020-11-29 21:52:16 +0000

    adding insomnia development and debug.sh

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, request, redirect
-from url_shortener import id_gen
+import string
+import random
+
 
 app = Flask(__name__)
+
+
+def id_gen(size=6, chars=string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 
 links = {}
 
 
-@app.route('/add')
+@app.route('/add', methods=['PUT'])
 def get_url():
     url = request.args.get('url', '')
     key = id_gen()
@@ -15,7 +21,10 @@ def get_url():
     return links
 
 
-@app.route('/search')
-def redirect_key():
-    key = request.args.get('key', '')
-    return redirect(str(links[key]))
+@app.route('/<id>', methods=['GET'])
+def redirect_key(id):
+    return redirect(str(links[id]))
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000)
--- url_shortener.py
+++
@@ -1,7 +0,0 @@
-# Importing the required libraries import contextlib
-import string
-import random
-
-
-def id_gen(size=6, chars=string.ascii_letters + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
