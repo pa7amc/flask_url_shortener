df9ada70b98561aa4b299989c9b06b25298f5fc8
pa7amc <pa7amc@gmail.com>
2020-11-30 16:11:42 +0000

    template inheritance and retrieving form data

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ def id_gen(size=6, chars=string.ascii_letters + string.digits):
 links = {}
 
 
-@app.route('/add', methods=['PUT'])
-def get_url():
-    url = request.args.get('url', '')
-    key = id_gen()
-    links[key] = url
-    return str(key) + " : " + str(links[key])
+# @app.route('/add', methods=['PUT'])
+# def get_url():
+#    url = request.args.get('url', '')
+#    key = id_gen()
+#    links[key] = url
+#    return str(key) + " : " + str(links[key])
 
 
 @app.route('/<id>', methods=['GET'])
@@ -26,19 +26,27 @@ def redirect_key(id):
     return redirect(str(links[id]))
 
 
-@app.route('/list', methods=['GET'])
+# @app.route('/listt', methods=['GET'])
+# def get_links():
+ #   return links
+
+
+@app.route('/list')
 def get_links():
-    return links
+    return render_template('list.html', links=links)
 
 
-@app.route('/listt')
-def listt():
-    return render_template('list.html', author='pat', links=links)
-
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html', author='pat')
+    # if hitting the submit post input
+    if request.method == 'POST':
+        url_ins = request.form["url"]
+        print(url_ins)
+        key = id_gen()
+        links[key] = url_ins
+        return str(key) + " : " + str(links[key])
+    else:
+        return render_template('home.html')
 
 
 if __name__ == '__main__':
